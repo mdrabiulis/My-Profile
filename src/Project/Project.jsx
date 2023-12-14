36484d4d2bf129bdf057276422dede79decd58f1
mdrabiulis <mdrabiulis155@gmail.com>
2023-12-15 00:53:58 +0600

    contact page add

--- src/Project/Project.jsx
+++ src/Project/Project.jsx
@@ -7,62 +7,16 @@ import img2 from '../assets/Screenshot_4.png'
 import { Link } from "react-router-dom";
 const Project = () => {
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 ml-8 mt-6 ">
+<div className=" pt-4 mr-10 overflow-auto h-[745px]">
+<h2 className="text-3xl font-bold my-2 pl-7 ">About Me</h2>
+    <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 ml-8 mt-4 ">
 
-<Link to={''}><ProjectCards imeags={img2} title={'Brandshop'}></ProjectCards></Link>
-<Link to={''}><ProjectCards imeags={img} title={'Online-Jobs-Marketplace'}></ProjectCards></Link>
+<Link to={'https://assignment-10-project-33ee9.web.app'} target="_blank"><ProjectCards imeags={img2} title={'Brandshop'}></ProjectCards></Link>
+<Link to={'https://ornate-strudel-d64653.netlify.app'} target="_blank"><ProjectCards imeags={img} title={'Online-Jobs-Marketplace'}></ProjectCards></Link>
 <Link to={'https://prismatic-biscochitos-7f1383.netlify.app' } target="_blank"><ProjectCards imeags={img1} title={'Fitness-Tracker'}></ProjectCards></Link>
 
-
-      {/* <Link className="">
-        <div className="w-[385px] h-[305px] bg-slate-500 ">
-          <div className="flex items-center justify-around ">
-            <h2 className="text-3xl">Brandshop</h2>
-            <img
-              src={img}
-              alt=""
-              className="w-[235px] h-[140px] rounded-md mt-3"
-            />
-          </div>
-        </div>
-      </Link>
-      <Link className="">
-        <div className="w-[385px] h-[305px] bg-slate-500 ">
-          <div className="flex items-center justify-around ">
-            <h2 className="text-3xl">Brandshop</h2>
-            <img
-              src={img}
-              alt=""
-              className="w-[235px] h-[140px] rounded-md mt-3"
-            />
-          </div>
-        </div>
-      </Link>
-      <Link className="">
-        <div className="w-[385px] h-[305px] bg-slate-500 ">
-          <div className="flex items-center justify-around ">
-            <h2 className="text-3xl">Brandshop</h2>
-            <img
-              src={img}
-              alt=""
-              className="w-[235px] h-[140px] rounded-md mt-3"
-            />
-          </div>
-        </div>
-      </Link>
-      <Link className="">
-        <div className="w-[385px] h-[305px] bg-slate-500 ">
-          <div className="flex items-center justify-around ">
-            <h2 className="text-3xl">Brandshop</h2>
-            <img
-              src={img}
-              alt=""
-              className="w-[235px] h-[140px] rounded-md mt-3"
-            />
-          </div>
-        </div>
-      </Link> */}
     </div>
+</div>
   );
 };
 
--- src/Project/ProjectCards.jsx
+++ src/Project/ProjectCards.jsx
@@ -4,7 +4,7 @@ const ProjectCards = ({ imeags, title }) => {
   return (
     <div>
       
-        <div className="w-[410px] h-[315px] card bg-base-100 shadow-xl ">
+        <div className="w-[380px] h-[315px] card bg-base-100 shadow-xl ">
           <div className=" items-center justify-around ">
             <img
               src={imeags}
