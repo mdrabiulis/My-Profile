59bc65e66267455a5de640223879f1e57df31632
mdrabiulis <mdrabiulis155@gmail.com>
2023-12-16 00:39:45 +0600

    contact page add

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,15 +1,20 @@
-
+import { useTypewriter, Cursor } from "react-simple-typewriter";
 
 const Home = () => {
-    return (
-        <div className="text-center text-7xl">
-            Home
-            Home
-            Home
-            Home
-            Home
-        </div>
-    );
+  const [text] = useTypewriter({
+    words: ["Front and Backend", "Developer"],
+    loop: 3,
+    
+  });
+  return (
+    <div className="">
+      <h1 className="text-center text-3xl font-bold mt-48">MD RABIUL ISLAM</h1>
+      <div className="App text-center">
+        <p className="text-xl">Hi, I am   <span>{text}</span><Cursor cursorColor="red" /></p>
+        
+      </div>
+    </div>
+  );
 };
 
 export default Home;
---
+++ src/Home/Ty.jsx
@@ -0,0 +1,13 @@
+
+
+const Ty = () => {
+
+    
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Ty;
